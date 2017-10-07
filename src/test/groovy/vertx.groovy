c330bd32992dca9623e3ee43dc7961058bd733d9
Mark Hudson <mark.github@mhudson.net>
2017-10-07 12:43:09 -0700

    dynamic output

--- src/test/groovy/vertx.groovy
+++ src/test/groovy/vertx.groovy
@@ -1,18 +1,16 @@
 #!/usr/bin/env groovy
 
-// simple web service for testing... mock Spock.groovy --by mHudson 2017.03
+// simple mock/stub web service  --by mHudson 2017.03
+// benchmark with:  wrk -c 2 -d 4 -t 2 http://localhost:8080
 
-@Grab("io.vertx:vertx-core:3.4.1")
+@Grab("io.vertx:vertx-core:3.4.2")
 import io.vertx.core.Vertx
 
 def port = this.args ? this.args[0].toInteger() : 8080
 
-io.vertx.core.http.HttpServerOptions options = [host:"0.0.0.0", port:port]
+options = [host:"0.0.0.0", port:port] as io.vertx.core.http.HttpServerOptions
 def server = Vertx.vertx().createHttpServer(options)
-def hrn=0
-def response_text = """
-{"requests":{"48":{"k":"v"} }}
-"""
+def request_count=0
 
 server.requestHandler({ request ->
     response = request.response()
@@ -21,12 +19,17 @@ server.requestHandler({ request ->
         ct = "text/plain"
     }
     response.putHeader("content-type", ct)  // "application/json")
-            .end(response_text)
-    println "HTTP request # ${hrn}:"
-    request.headers().each{ println "    " + it }
-    println "HTTP response # ${hrn}:"
-    response.headers().each{ println "    " + it }
-    hrn += 1
+            .end("""
+  {"requests":{"${request_count}":{"k":"v"} }}
+""") // added linefeed prefix/suffix for user-friendly human-readability
+
+    print   "HTTP request# ${request_count}: "
+    request.headers().each{  print "${it}\t" }
+    print "\n    response# ${request_count}: "
+    response.headers().each{ print "${it}\t" }
+    print "\n"
+    request_count += 1
 }).listen()
 
 println "Started on port ${options.port}"
+
