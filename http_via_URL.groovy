42b1157e9bbd36247edde28c874399d16b7b78d1
Mark Hudson <mark@gem.io>
2017-10-30 12:03:25 -0700

    added JSON parsing

--- http_via_URL.groovy
+++ http_via_URL.groovy
@@ -7,8 +7,11 @@ import spock.lang.*
 class MyFirstSpec extends Specification {
   def "let's try this!"() {
     when: "call a path that returns JSON"
-      def response = "https://httpbin.org/get".toURL()
-      println response.text
+      // def response = "https://httpbin.org/get".toURL()  // Groovy
+      def response = new URL("http://httpbin.org/get")  // implicit java.net.URL
+      def json_map = new groovy.json.JsonSlurper().parseText(response.text)  // getText())
+      println "raw response.text: ${response.text}"
+      println "JSON parseText: ${json_map}"
     then:
       response.text =~ /"args"/
       response.text =~ /"headers"/
