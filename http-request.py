ac32deb90b0c02f6def7d4a563def3ddd77a17ee
Mark Hudson <mark@gem.co>
2018-05-02 11:02:59 -0700

    example of Python's HTTP Requests

---
+++ http-request.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import json
+import requests
+
+r = requests.get("http://example.com/")
+
+print("GET " + r.url + " returned Content-Length: {}".format(r.headers["content-length"]))
+print("...and all the rest of the headers:")
+print(json.dumps(dict(r.headers),sort_keys=True,indent=2))
