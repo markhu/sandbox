0ec9eaaa6c638688ea7bdd4489def2bfba632cd1
Mark Hudson <mark.github@mhudson.net>
2022-07-27 16:28:48 -0700

    add argv to http-request.py

--- http-request.py
+++ http-request.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import json
 import requests  # HTTP Requests lib
+import sys
 
-r = requests.get("http://example.com/")
+url = sys.argv[1] if len(sys.argv)>1 else "http://example.com/"
+url = f"http://{url}" if "http" not in url else url
+r = requests.get(url)
 
-print("GET " + r.url + " returned Content-Length: {}".format(r.headers["content-length"]))
-print("...and all the rest of the headers:")
+print("GET " + r.url + " returned Content-Length: {}".format(r.headers.get("Content-Length")))
+print("...and these headers:")
 print(json.dumps(dict(r.headers),sort_keys=True,indent=2))
